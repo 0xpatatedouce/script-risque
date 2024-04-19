f98e65a25c4dd7ba15ee9b20bd422bf84562c60a
patatedouce <123324096+0xpatatedouce@users.noreply.github.com>
2024-04-19 20:55:08 +0200

    Update scriptRz.sh

--- scriptRz.sh
+++ scriptRz.sh
@@ -6,19 +6,14 @@ exists()
 if exists curl; then
 echo ''
 else
-  sudo apt update && sudo apt install curl -y < "/dev/null"
+  sudo apt update && sudo apt install curl screen -y < "/dev/null"
 fi
-
 
 # Télécharger le script d'installation de NVM depuis GitHub
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 
 # Installer NVM en exécutant le script d'installation
 sh install_nvm.sh
-
-# Si l'installation de NVM nécessite d'accepter des conditions de licence, suivre les instructions pour accepter
-# Par exemple, sur macOS, cela peut nécessiter d'exécuter la commande suivante
-# sudo xcodebuild -license
 
 # Installer la version spécifique de NodeJS (16.20) avec NVM
 nvm install 16.20
@@ -31,3 +26,12 @@ mkdir ~/p0tion-tmp
 
 # Naviguer vers le répertoire temporaire créé
 cd ~/p0tion-tmp
+
+# Installer le package @p0tion/phase2cli via npm
+npm i @p0tion/phase2cli
+
+# Démarrer une nouvelle session screen nommée "p0tion"
+screen -S p0tion
+
+# Exécuter la commande 'npx phase2cli auth'
+npx phase2cli auth
